a143432b4ec3b7ce2d07a64417677cea6beebf50
mohamed <2305273@anu.edu.eg>
2024-12-12 01:51:31 +0200

    final code before goin sleep

--- connect4.py
+++ connect4.py
@@ -23,26 +23,29 @@ player_1 = 1
 player_2 = 2
 
 players = [player_1,player_2]
+
+# creating a board from numpy
 def create_board():
     board =  np.zeros((6,7))
     return board
 
-def drop_piece(board,selec,row, no_of_player ):
+def drop_piece(board,selec,row, no_of_player):
     board[row][selec] = no_of_player
 
 def is_loc_valid(board,selec):
 
-    if board[5][selec] == 0 :
+    if board[0][selec] == 0 :
         return True
     else: 
-        print("col is full") 
+        label = my_font.render("column is full!", 1, (139,0,0))
+        screen.blit(label, (40, 10))
         return False
     
 
 def get_open_valid_pos(board,selec):
-    for row in range(6):
-        if board[row][selec] == 0:
-            return row
+    for r in range(6-1, -1, -1):
+        if board[r][selec] == 0:
+            return r
 
 
 
@@ -73,11 +76,14 @@ def draw_board(board):
             if board[r][c] == 0:
                 pygame.draw.circle(screen, empty_circle, (int(c * size + size/2), int(r* size + size + size/2)), circle_radius)
             elif board[r][c] == 1:
-                pygame.draw.circle(screen, player_2_color, (int(c * size + size/2), int(r* size + size + size/2)), circle_radius)
-            else :
                 pygame.draw.circle(screen, player_1_color, (int(c * size + size/2), int(r* size + size + size/2)), circle_radius)
+            else :
+                pygame.draw.circle(screen, player_2_color, (int(c * size + size/2), int(r* size + size + size/2)), circle_radius)
 
     pygame.display.update()
+
+
+
 
 board = create_board()
 game_over = False
@@ -99,7 +105,7 @@ size  = 100
 width = 100 * 7
 height = 100 * (6 + 1)
 
-circle_radius = int(size/2 - 5)
+circle_radius = int(size/2 - 5) # 100/2 - 5 = 45
 
 screen = pygame.display.set_mode((width , height))
 draw_board(board)
@@ -117,10 +123,11 @@ while not game_over:
         if event.type == pygame.MOUSEMOTION and not_over:
             pygame.draw.rect(screen, bord_color, (0, 0, width, size))
             xpos = pygame.mouse.get_pos()[0]
+            # turn 0 => player 1
             if turn == 0:
-                pygame.draw.circle(screen, player_2_color, (xpos, int(size/2)), circle_radius )
-            else: 
                 pygame.draw.circle(screen, player_1_color, (xpos, int(size/2)), circle_radius )
+            else: 
+                pygame.draw.circle(screen, player_2_color, (xpos, int(size/2)), circle_radius )
 
         pygame.display.update()
 
