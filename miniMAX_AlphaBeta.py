9e87627f3d9e90e16980957e13445e6fda632d12
mohamed <2305273@anu.edu.eg>
2024-12-13 23:59:39 +0200

    add miniMax_AlphaBeta.py without minimax algorithm xD >_<

--- miniMAX_AlphaBeta.py
+++ miniMAX_AlphaBeta.py
@@ -29,12 +29,12 @@ def create_board():
     board =  np.zeros((6,7))
     return board
 
-def drop_piece(board,col,row, no_of_player):
-    board[row][col] = no_of_player
+def drop_piece(board,selec,row, no_of_player):
+    board[row][selec] = no_of_player
 
-def is_loc_valid(board,col):
+def is_loc_valid(board,selec):
 
-    if board[0][col] == 0 :
+    if board[0][selec] == 0 :
         return True
     else: 
         label = my_font.render("column is full!", 1, (139,0,0))
@@ -42,9 +42,9 @@ def is_loc_valid(board,col):
         return False
     
 
-def get_open_valid_pos(board, col):
-    for r in range(5, -1, -1): # 5 4 3 2 1 0 
-        if board[r][col] == 0: #  2 
+def get_open_valid_pos(board,selec):
+    for r in range(6-1, -1, -1):
+        if board[r][selec] == 0:
             return r
 
 def check_win(row, col):
@@ -67,6 +67,62 @@ def check_win(row, col):
                 return True
         return False
 
+def evaluate_window(window, piece):
+	score = 0
+	opp_piece = 1
+	if piece == 1:
+		opp_piece = 2
+
+	if window.count(piece) == 4:
+		score += 100
+	elif window.count(piece) == 3 and window.count(0) == 1:
+		score += 5
+	elif window.count(piece) == 2 and window.count(0) == 2:
+		score += 2
+
+	if window.count(opp_piece) == 3 and window.count(0) == 1:
+		score -= 4
+
+	return score
+
+def score_position(board, piece):
+	score = 0
+
+	## Score center column
+	center_array = [int(i) for i in list(board[:, 7//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 3
+
+	## Score Horizontal
+	for r in range(6):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(7-3):
+			window = row_array[c:c + 4]
+			score += evaluate_window(window, piece)
+
+	## Score Vertical
+	for c in range(7):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(6-3):
+			window = col_array[r:r+4]
+			score += evaluate_window(window, piece)
+
+	## Score posiive sloped diagonal
+	for r in range(6-3):
+		for c in range(7-3):
+			window = [board[r+i][c+i] for i in range(4)]
+			score += evaluate_window(window, piece)
+
+	for r in range(6-3):
+		for c in range(7-3):
+			window = [board[r+3-i][c+i] for i in range(4)]
+			score += evaluate_window(window, piece)
+
+	return score
+
+def minimaxAlphaBeta():
+      pass
+
 def draw_board(board):
     for c in range(7):
         for r in range(6):
@@ -80,8 +136,15 @@ def draw_board(board):
 
     pygame.display.update()
 
+def is_terminal_node(board):
+	return check_win(board, 1) or check_win(board, 2) or len(get_valid_locations(board)) == 0
 
-
+def get_valid_locations(board):
+	valid_locations = []
+	for col in range(7):
+		if is_loc_valid(board, col):
+			valid_locations.append(col)
+	return valid_locations
 
 board = create_board()
 game_over = False
@@ -124,10 +187,6 @@ while not game_over:
             # turn 0 => player 1
             if turn == 0:
                 pygame.draw.circle(screen, player_1_color, (xpos, int(size/2)), circle_radius )
-            else: 
-                pygame.draw.circle(screen, player_2_color, (xpos, int(size/2)), circle_radius )
-
-        pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN and not_over:
 
@@ -140,7 +199,6 @@ while not game_over:
                 if is_loc_valid(board,selec):
                     row = get_open_valid_pos(board,selec)
                     drop_piece(board,selec,row, player_1 )
-             
                     if check_win(row, selec):
                         print("PLAYER 1 WINS!")
                         label = my_font.render("PLAYER 1 WINS!", 1, (139,0,0))
@@ -148,30 +206,39 @@ while not game_over:
                         not_over = False
                         t = Timer(3.0, end_game)
                         t.start()
+
+                draw_board(board) 
+
+                # increment turn by 1
+                turn += 1
+
+                # this will alternate between 0 and 1 withe very turn
+                turn = turn % 2 
+
+        pygame.display.update()
         
-            else:
-                # we assume players will use correct input
-                xpos = event.pos[0] 
-                selec = int(math.floor(xpos/size)) #int(input("Player 1 make your selection by typing (0-6):"))
+    if turn == 1 and not_over and not game_over:
 
-                if is_loc_valid(board,selec):
-                    row = get_open_valid_pos(board,selec)
-                    drop_piece(board,selec,row, player_2 )
+        col, minimax_score = minimaxAlphaBeta()
+
+        if is_loc_valid(board,selec):
+            pygame.time.wait(500)
+            row = get_open_valid_pos(board,selec)
+            drop_piece(board,selec,row, player_2 )
              
-                    if check_win(row, selec):
-                        print("PLAYER 2 WINS!")
-                        label = my_font.render("PLAYER 2 WINS!", 1, (139,0,0))
-                        screen.blit(label, (40, 10))
-                        not_over = False
-                        t = Timer(3.0, end_game)
-                        t.start()
+            if check_win(row, selec):
+                print("PLAYER 2 WINS!")
+                label = my_font.render("PLAYER 2 WINS!", 1, (139,0,0))
+                screen.blit(label, (40, 10))
+                not_over = False
+                t = Timer(3.0, end_game)
+                t.start()
 
-            draw_board(board)
+        draw_board(board)
 
-            # increment turn by 1
-            turn += 1
-            # this will alternate between 0 and 1 withe very turn
-            turn = turn % 2
+        # increment turn by 1
+        turn += 1
+         # this will alternate between 0 and 1 withe very turn
+        turn = turn % 2
 
-
-
+draw_board(board)
